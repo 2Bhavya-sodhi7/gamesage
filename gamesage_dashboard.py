6a266f91ea81f5d77864903ef447cc4e5a6e08e0
2Bhavya-sodhi7 <153315787+2Bhavya-sodhi7@users.noreply.github.com>
2025-06-18 19:10:40 +0530

    Update gamesage_dashboard.py

--- gamesage_dashboard.py
+++ gamesage_dashboard.py
@@ -12,7 +12,7 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
 # 1. Page configuration MUST be first
-st.set_page_config(page_title="GameSage", layout="wide", page_icon="🎯")
+st.set_page_config(page_title="GameSage", layout="wide", page_icon="🎯",initial_sidebar_state="expanded")
 # 3. Sidebar navigation
 menu = st.sidebar.selectbox("Navigate", [
     "Home",
